f8ffc731030c567379366be4787f4e4da9538aad
seonjae <tjswo2292@naver.com>
2023-09-05 22:02:07 +0900

    feat: Router 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '../components/layout/Layout'
+import Main from '../components/search'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>{/* <Route path='/' element={}/> */}</Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
