75cfe069188f211a76c7d6eb577054eb2cf9ef36
seonjae <tjswo2292@naver.com>
2023-09-06 11:15:40 +0900

    feat: input useDebounce 커스텀 훅 추가

---
+++ src/components/search/hook/useDebounce.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const useDebounce = () => {
+  const [timer, setTimer] = useState<ReturnType<typeof setTimeout>>()
+
+  const dispatchDebounce = (callback: () => void) => {
+    const delay = 500
+
+    if (timer) {
+      clearTimeout(timer)
+    }
+
+    const newTimer = setTimeout(() => {
+      callback()
+    }, delay)
+
+    setTimer(newTimer)
+  }
+
+  return [dispatchDebounce]
+}
+
+export default useDebounce
