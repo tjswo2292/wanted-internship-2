cbb9d64869180af6d9ef85eeecfa61475676e4d6
seonjae <tjswo2292@naver.com>
2023-09-09 18:21:06 +0900

    design: recommend 폴더에서 공토 사용되는 스타일 분리

---
+++ src/components/recommend/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const TitleWrapper = styled.div`
+  margin: 2rem 2rem 1.5rem 2rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #888;
+`
+export const Title = styled.span``
+export const NothingWrapper = styled.div`
+  margin-top: 2rem;
+`
+export const Nothing = styled.span``
+export const RecentList = styled.ol`
+  height: 5;
+`
