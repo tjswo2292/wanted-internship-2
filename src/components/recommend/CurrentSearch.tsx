eefc25ce1e0cfdc0c4f73282dee58da27a245fb2
seonjae <tjswo2292@naver.com>
2023-09-16 15:18:59 +0900

    remove: 불필요한 코드 삭제

--- src/components/recommend/CurrentSearch.tsx
+++ src/components/recommend/CurrentSearch.tsx
@@ -3,23 +3,21 @@ import { RecentList, Title, TitleWrapper } from './style'
 
 import { useAppSelector } from '../../store/hooks'
 import IncludeSearch from './IncludeSearch'
-import { IncludeKeywordItemType } from '../../store/slice/searchSlice'
 
 const CurrentSearch = () => {
   const { includeKeywordList, searchQuery } = useAppSelector(
     (state) => state.search,
   )
+
   return (
     <>
       <TitleWrapper>
         <Title>{searchQuery}</Title>
       </TitleWrapper>
       <RecentList>
-        {includeKeywordList.map(
-          ({ sickCd, sickNm }: IncludeKeywordItemType) => (
-            <IncludeSearch key={sickCd} text={sickNm} />
-          ),
-        )}
+        {includeKeywordList.map(({ sickCd, sickNm }) => (
+          <IncludeSearch key={sickCd} text={sickNm} />
+        ))}
       </RecentList>
     </>
   )
